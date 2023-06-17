1910dbd9370fe1de6f7a16d4d72813d849e57629
ShanHng <hngshanhern@gmail.com>
2023-06-17 16:43:56 +0800

    Complete function playRound

--- script.js
+++ script.js
@@ -17,3 +17,50 @@ function getComputerChoice() {
             return "Scissors";
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+    
+    // parse both player's and computer's input 
+    const playerSelectionLowerCase = playerSelection.toLowerCase();
+    const computerSelectionLowerCase = computerSelection.toLowerCase();
+
+    // process inputs of game. main logic of the game goes here
+    // store outcome of game in a variable. outcome of game is either 1, 0 or -1.
+    let outcome;
+    switch (playerSelectionLowerCase) {
+        case("rock"):
+            outcome = computerSelectionLowerCase === "rock" 
+            ? 0 : (computerSelectionLowerCase === "scissors" ? 1 : -1)
+            break;
+
+        case("paper"):
+            outcome = computerSelectionLowerCase === "paper" 
+            ? 0 : (computerSelectionLowerCase === "rock" ? 1 : -1)
+            break;
+
+        case("scissors"):
+            outcome = computerSelectionLowerCase === "scissors" 
+            ? 0 : (computerSelectionLowerCase === "paper" ? 1 : -1)
+            break;
+    }
+
+    // return message corresponding to game scenario
+    formattedPlayerSelection = playerSelectionLowerCase.charAt(0).toUpperCase() + playerSelectionLowerCase.slice(1);
+    formattedComputerSelection = computerSelectionLowerCase.charAt(0).toUpperCase() + computerSelectionLowerCase.slice(1);
+    const MESSAGE_WIN = `You win! ${formattedPlayerSelection} beats ${formattedComputerSelection}`  
+    const MESSAGE_DRAW = `A draw. Computer chose ${formattedComputerSelection}. Try again!`
+    const MESSAGE_LOSE = `You lose! ${formattedComputerSelection} beats ${formattedPlayerSelection}`  
+    switch(outcome) {
+        case 0:
+            return MESSAGE_DRAW;
+
+        case -1:
+            return MESSAGE_LOSE;
+
+        case 1:
+            return MESSAGE_WIN;
+
+    }
+}
+   
